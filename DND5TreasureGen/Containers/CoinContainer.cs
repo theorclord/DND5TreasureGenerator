f2f7510ef419a5442e7b462bd456db9e39620100
Mikkel Stolborg <mikkel.stolborg@capgemini.com>
2021-08-05 17:01:39 +0200

    Added simple dice container and gold random logic

---
+++ DND5TreasureGen/Containers/CoinContainer.cs
@@ -0,0 +1,27 @@
+﻿using DND5TreasureGen.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND5TreasureGen.Containers
+{
+    public class CoinContainer
+    {
+        public Dice CopperDice { get; set; }
+        public Dice SilverDice { get; set; }
+        public Dice GoldDice { get; set; }
+        public Dice PlatinumDice { get; set; }
+
+        public string GetCoinAmount()
+        {
+            int copper = CopperDice.Roll();
+            int silver = SilverDice.Roll();
+            int gold = GoldDice.Roll();
+            int platinum = PlatinumDice.Roll();
+            return $"PP: {platinum}, GP: {gold}, SP: {silver}, CP: {copper}";
+        }
+
+    }
+}
